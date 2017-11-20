4658c328ccf9df60e82adde4f9527073464d0a3b
ARTSIOM KUKHAREV <xeontem@gmail.com>
2017-11-20 12:57:04 +0300

    circle/fix: fixed forms behavior

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,18 +10,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class HeaderComponent implements OnInit {
   // template driven form
-  firstname = 'artsiom';
-  lastname = 'kukharev';
-  // selectedValue: string
+  firstname: string = 'artsiom';
+  lastname: string = 'kukharev';
+  
+  selectedValue: string
   selectedFood: FormGroup
   
   // reactive form build
-  foods: {value: string, viewValue: string}[] =
+  foods: {value: string, viewValue: string, hint: string}[] =
   [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
-    {value: 'tacos-3', viewValue: 'Tacos2'}
+    {value: 'steak-0', viewValue: 'Steak', hint: 'it\'s a hint'},
+    {value: 'pizza-1', viewValue: 'Pizza', hint: 'it\'s a hint'},
+    {value: 'tacos-2', viewValue: 'Tacos', hint: 'it\'s a hint'},
+    {value: 'tacos-3', viewValue: 'Tacos2', hint: 'it\'s a hint'}
   ];
 
   constructor(private fb: FormBuilder) { }
@@ -29,11 +30,18 @@ export class HeaderComponent implements OnInit {
   ngOnInit():void {
     // reactive form build
     this.selectedFood = this.fb.group({
-      reactValue: ''
+      reactValue: ['', Validators.required]
     });
   }
 
-  logForm(value: any) {
+  tplDrivenSubmit(value: any) {
     console.log(value);
+  }
+
+  reactiveSubmit(value: any) {
+    console.log(value);
+  }
+  checkValue() {
+    console.log(this.selectedFood);
   }
 }

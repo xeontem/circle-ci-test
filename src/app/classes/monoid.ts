eff418c94e9d3c97f8440622086dd4c184093848
xeontem <xeontem@gmail.com>
2017-11-21 00:14:20 +0300

    circle/feature: algebraic data types

--- src/app/classes/monoid.ts
+++ src/app/classes/monoid.ts
@@ -1,2 +1,16 @@
-export class Monoid {
+export default class Monoid {
+  value: any;
+
+  constructor(value?: any) {
+    this.value = value === undefined ? [] : [value];
+  }
+
+  empty() {
+    return new Monoid();
+  }
+
+  concat(monoid: Monoid) {
+    return this.value.concat();
+  }
+
 }

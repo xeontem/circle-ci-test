dd8cd4e4fb159c28dac346f2a1259f52be66dd58
xeontem <xeontem@gmail.com>
2018-01-02 12:37:15 +0300

    circle: monoid refactored

--- src/app/classes.old/monoid.ts
+++ src/app/classes.old/monoid.ts
@@ -1,16 +1,24 @@
 export default class Monoid {
-  value: any;
+  private value: any[];
 
   constructor(value?: any) {
     this.value = value === undefined ? [] : [value];
   }
 
-  empty() {
+  private empty() {
     return new Monoid();
   }
 
-  concat(monoid: Monoid) {
-    return this.value.concat();
+  concat(monoid: Monoid): Monoid {
+    return new Monoid(this.value.concat(monoid.value))
+    // return new Monoid(this.value.concat());
   }
 
 }
+
+
+const mon1: Monoid = new Monoid(23);
+const mon2: Monoid = new Monoid('hello');
+const mon3: Monoid = mon1.concat(mon2);
+
+console.log(mon3);

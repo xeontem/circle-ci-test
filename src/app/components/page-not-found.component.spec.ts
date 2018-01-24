f934c5fc9b0050697168666a5700c0af85ae3275
Artsiom Kukharev EPAM <artsiom_kukharev@epam.com>
2018-01-24 14:32:39 +0300

    fix: fixed pagenotfound component test

--- src/app/components/page-not-found.component.spec.ts
+++ src/app/components/page-not-found.component.spec.ts
@@ -1,12 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PageNotFoundComponent } from './page-not-found.component';
 
+import { MatToolbarModule } from '@angular/material';
+
 describe('PageNotFoundComponentComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ MatToolbarModule ],
       declarations: [ PageNotFoundComponent ]
     })
     .compileComponents();

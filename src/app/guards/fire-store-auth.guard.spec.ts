bca431a5623c32b8740ea7488dfcbe1b8328e4cb
Artsiom Kukharev EPAM <artsiom_kukharev@epam.com>
2018-01-24 14:35:35 +0300

    fix: fixed fcmauthguard guard test

--- src/app/guards/fire-store-auth.guard.spec.ts
+++ src/app/guards/fire-store-auth.guard.spec.ts
@@ -1,11 +1,23 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { FirestoreAuthService } from '../services/firestore-auth.service';
+
+//-------------------------------- stubs --------------------------------
+import {
+  RouterStub,
+  FirestoreAuthServiceStub,
+} from '../../../tests/stubs';
 
 import { FireStoreAuthGuard } from './fire-store-auth.guard';
 
 describe('FireStoreAuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FireStoreAuthGuard]
+      providers: [
+        FireStoreAuthGuard,
+        {provide: Router, useClass: RouterStub },
+        {provide: FirestoreAuthService, useClass: FirestoreAuthServiceStub },
+      ]
     });
   });
 

ab987b57046752cbc7aa2536291725558dcf3c0f
Artsiom Kukharev EPAM <artsiom_kukharev@epam.com>
2018-01-24 17:57:49 +0300

    fix: not finished yet cource component test

--- src/app/modules/cources/components/cource.component.spec.ts
+++ src/app/modules/cources/components/cource.component.spec.ts
@@ -1,6 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { CourceComponent } from './cource.component';
+// import { DurationPipe } from '../pipes/duration.pipe';
+import {
+  MatIconModule,
+  MatFormFieldModule,
+  MatSliderModule,
+  MatDatepickerModule,
+
+  MatCardModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatButtonModule,
+  MatFormFieldControl,
+  MatDialogModule,
+  MatNativeDateModule,
+  MatProgressSpinnerModule,
+  MatSelectModule
+} from '@angular/material';
+
+import { Pipe, PipeTransform } from '@angular/core';
+import { getInt, getFraction, toHalfHour } from '../../../tools/lambda';
+@Pipe({ name: 'duration' })
+class DurationPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    return value;
+  }
+
+}
 
 describe('CourceComponent', () => {
   let component: CourceComponent;
@@ -8,7 +35,16 @@ describe('CourceComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourceComponent ]
+      declarations: [ CourceComponent, DurationPipe ],
+      imports: [
+        ReactiveFormsModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatSliderModule,
+        MatIconModule,
+      ],
+      providers: [
+      ]
     })
     .compileComponents();
   }));
